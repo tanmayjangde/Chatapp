5ba78fd7ee28814efee2d28941696e57d658b56c
tanmayjangde <tanmayjangde7@gmail.com>
2021-05-26 01:55:14 +0530

    Authentication applied,Signup and Splash Activity added. Ui for sign up created.

--- app/src/main/java/com/tanmay/chatapp/OtpActivity.kt
+++ app/src/main/java/com/tanmay/chatapp/OtpActivity.kt
@@ -1,7 +1,8 @@
 package com.tanmay.chatapp
 
+import android.app.ProgressDialog
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.text.SpannableString
@@ -9,63 +10,277 @@ import android.text.Spanned
 import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_otp.*
+import java.util.concurrent.TimeUnit
+
 
 const val PHONE_NUMBER = "phoneNumber"
-class OtpActivity : AppCompatActivity() {
-    var phoneNumber:String?=null
+
+class OtpActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    private var phoneNumber: String? = null
+    private var mVerificationId: String? = null
+    private var mResendToken: PhoneAuthProvider.ForceResendingToken? = null
+    private lateinit var progressDialog: ProgressDialog
+    private var mCounterDown: CountDownTimer? = null
+    private var timeLeft: Long = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
-        initViews()
+        initView()
+        startVerify()
+    }
+
+    private fun startVerify() {
+        startPhoneNumberVerification(phoneNumber!!)
         showTimer(60000)
+        progressDialog = createProgressDialog("Sending a verification code", false)
+        progressDialog.show()
     }
 
-    private fun showTimer(milliSecInFuture: Long) {
-        resendCodeBtn.isEnabled=false
-        object :CountDownTimer(milliSecInFuture,1000){
-            override fun onTick(milliSecUntilFinished: Long) {
-                counterTv.isVisible = true
-                counterTv.text = getString(R.string.secondsRemaining,milliSecUntilFinished/1000)
-            }
-
-            override fun onFinish() {
-                counterTv.isVisible = false
-                resendCodeBtn.isEnabled=true
-            }
-        }.start()
-    }
-
-    private fun initViews() {
-        phoneNumber=intent.getStringExtra(PHONE_NUMBER)
-        verify.setText("""Verify $phoneNumber""")
+    private fun initView() {
+        phoneNumber = intent.getStringExtra(PHONE_NUMBER)
+        verify.text = "Verify ${phoneNumber}"
         setSpannableString()
+
+        sendVeriCodeBtn.setOnClickListener(this)
+        resendCodeBtn.setOnClickListener(this)
+
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+
+                if (::progressDialog.isInitialized) {
+                    progressDialog.dismiss()
+                }
+
+                val smsMessageSent = credential.smsCode
+                if (!smsMessageSent.isNullOrBlank())
+                    sendcodeEt.setText(smsMessageSent)
+
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+
+                if (::progressDialog.isInitialized) {
+                    progressDialog.dismiss()
+                }
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+
+                    Log.e("Exception:", "FirebaseAuthInvalidCredentialsException", e)
+                    Log.e("=========:", "FirebaseAuthInvalidCredentialsException " + e.message)
+
+
+                } else if (e is FirebaseTooManyRequestsException) {
+
+                    Log.e("Exception:", "FirebaseTooManyRequestsException", e)
+                }
+
+
+                notifyUserAndRetry("Your Phone Number might be wrong or connection error.Retry again!")
+
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                progressDialog.dismiss()
+                counterTv.isVisible = false
+
+                Log.e("onCodeSent==", "onCodeSent:$verificationId")
+
+                mVerificationId = verificationId
+                mResendToken = token
+
+            }
+        }
     }
 
     private fun setSpannableString() {
-        val span = SpannableString("Waiting to automatically detect an otp sent to ${phoneNumber}. Wrong number?")
-        val clickableSpan = object : ClickableSpan(){
-            override fun onClick(widget: View) {
-
+        val span = SpannableString("Waiting to automatically detect otp to ${phoneNumber}. Wrong number?")
+        val clickSpan: ClickableSpan = object : ClickableSpan() {
+            override fun updateDrawState(ds: TextPaint) {
+                ds.color = ds.linkColor
+                ds.isUnderlineText = false
             }
 
-            override fun updateDrawState(ds: TextPaint) {
-                super.updateDrawState(ds)
-                ds.isUnderlineText = false
-                ds.color = ds.linkColor
+            override fun onClick(textView: View) {
+                showLoginActivity()
             }
         }
-        span.setSpan(clickableSpan,span.length-13,span.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        span.setSpan(clickSpan, span.length - 13, span.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         waitingTv.movementMethod = LinkMovementMethod.getInstance()
         waitingTv.text = span
     }
 
-    private fun showLoginctivity(){
-        startActivity(Intent(this,LoginActivity::class.java)
-            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK))
+    private fun notifyUserAndRetry(message: String) {
+        MaterialAlertDialogBuilder(this).apply {
+            setMessage(message)
+            setPositiveButton("Ok") { _, _ ->
+                showLoginActivity()
+            }
+
+            setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+            setCancelable(false)
+            create()
+            show()
+        }
     }
 
-    override fun onBackPressed() {}
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putLong("timeLeft", timeLeft)
+        outState.putString(PHONE_NUMBER, phoneNumber)
+    }
+
+    // This method will send a code to a given phone number as an SMS
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        val options = PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
+            .setPhoneNumber(phoneNumber)       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this)                 // Activity (for callback binding)
+            .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private fun showLoginActivity() {
+        startActivity(
+            Intent(this, LoginActivity::class.java)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        )
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        val mAuth = FirebaseAuth.getInstance()
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    if (::progressDialog.isInitialized) {
+                        progressDialog.dismiss()
+                    }
+                    //First Time Login
+                    if (task.result?.additionalUserInfo?.isNewUser == true) {
+                        showSignUpActivity()
+                    } else {
+                        showHomeActivity()
+                    }
+                } else {
+
+                    if (::progressDialog.isInitialized) {
+                        progressDialog.dismiss()
+                    }
+
+                    notifyUserAndRetry("Your Phone Number Verification is failed.Retry again!")
+                }
+            }
+    }
+
+    private fun showSignUpActivity() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun showHomeActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onBackPressed() {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mCounterDown != null) {
+            mCounterDown!!.cancel()
+        }
+    }
+
+    override fun onClick(v: View) {
+        when (v) {
+            sendVeriCodeBtn-> {
+                var code = sendcodeEt.text.toString()
+                if (code.isNotEmpty() && !mVerificationId.isNullOrEmpty()) {
+
+                    progressDialog = createProgressDialog("Please wait...", false)
+                    progressDialog.show()
+                    val credential =
+                        PhoneAuthProvider.getCredential(mVerificationId!!, code.toString())
+                    signInWithPhoneAuthCredential(credential)
+                }
+            }
+
+            resendCodeBtn -> {
+                if (mResendToken != null) {
+                    resendVerificationCode(phoneNumber.toString(), mResendToken)
+                    showTimer(60000)
+                    progressDialog = createProgressDialog("Sending a verification code", false)
+                    progressDialog.show()
+                } else {
+                    Toast.makeText(this,"Sorry, You Can't request new code now, Please wait ...",LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun showTimer(milliesInFuture: Long) {
+        resendCodeBtn.isEnabled = false
+        mCounterDown = object : CountDownTimer(milliesInFuture, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished
+                counterTv.isVisible = true
+                counterTv.text = "Seconds remaining: " + millisUntilFinished / 1000
+            }
+
+            override fun onFinish() {
+                resendCodeBtn.isEnabled = true
+                counterTv.isVisible = false
+            }
+        }.start()
+    }
+
+    private fun resendVerificationCode(
+        phoneNumber: String,
+        mResendToken: PhoneAuthProvider.ForceResendingToken?
+    ) {
+        val options = PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(callbacks)
+            .setForceResendingToken(mResendToken!!)
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+}
+
+fun Context.createProgressDialog(message: String, isCancelable: Boolean): ProgressDialog {
+    return ProgressDialog(this).apply {
+        setCancelable(isCancelable)
+        setCanceledOnTouchOutside(false)
+        setMessage(message)
+    }
 }
---
+++ app/src/main/java/com/tanmay/chatapp/User.kt
@@ -0,0 +1,25 @@
+package com.tanmay.chatapp
+
+import com.google.firebase.firestore.FieldValue
+
+data class User(
+    val name: String,
+    val imageUrl: String,
+    val thumbImage:String,
+    val uid: String,
+    val deviceToken: String,
+    val status: String,
+    val onlineState: String
+){
+    constructor(): this("","","","","","","")
+
+    constructor(name: String,imageUrl: String,thumbImage: String,uid: String) : this(
+        name,
+        imageUrl,
+        thumbImage,
+        uid,
+        "",
+        "Hey there I am using Chatapp",
+        ""
+    )
+}
